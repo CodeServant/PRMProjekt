608cf6106e9ff0e9086f7b878acf217eaeda20e2
CodeServant <49171565+CodeServant@users.noreply.github.com>
2023-05-20 19:10:26 +0200

    film default values

--- app/src/main/java/com/example/prmprojekt/Film.kt
+++ app/src/main/java/com/example/prmprojekt/Film.kt
@@ -2,4 +2,4 @@ package com.example.prmprojekt
 
 import java.math.BigDecimal
 
-data class Film (var nazwa: String, var rating: BigDecimal, var id: Int, var url: String = "")
+data class Film (var nazwa: String="", var rating: BigDecimal?=null, var id: Int, var url: String = "")
