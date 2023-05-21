7a06b7082af5b3075361a3fab9ce5c199537fe39
CodeServant <49171565+CodeServant@users.noreply.github.com>
2023-05-21 16:54:39 +0200

    selected item now can be deleted properly

--- app/src/main/java/com/example/prmprojekt/ListScreen.kt
+++ app/src/main/java/com/example/prmprojekt/ListScreen.kt
@@ -1,8 +1,7 @@
 package com.example.prmprojekt
 
-import android.content.Context
-import android.widget.Toast
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,19 +12,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,23 +28,30 @@ import androidx.navigation.NavController
 
 
 @Composable
-fun ListScreen(navController: NavController, films:MutableList<Film>){
+fun ListScreen(navController: NavController, films: MutableList<Film>) {
 
     val context = LocalContext.current
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = { navController.navigate(NavDestination.Add.route) },
-                backgroundColor = Color(0xff64cc30))
+                backgroundColor = Color(0xff64cc30)
+            )
             {
                 Icon(Icons.Default.Add, contentDescription = null)
             }
         }
     ) {
-        Column(modifier = Modifier.padding(it).fillMaxHeight(), verticalArrangement = Arrangement.SpaceBetween) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
 
-            FilmList(films = films, navController= navController)
-            Text(text = "${context.getString(R.string.quantity_sum)} ${films.size}",
+            FilmList(films = films, navController = navController)
+            Text(
+                text = "${context.getString(R.string.quantity_sum)} ${films.size}",
                 fontSize = 30.sp,
                 modifier = Modifier.padding(15.dp)
             )
@@ -59,22 +61,43 @@ fun ListScreen(navController: NavController, films:MutableList<Film>){
 }
 
 @Composable
-fun FilmList(navController: NavController, films: List<Film>) {
+fun FilmList(navController: NavController, films: MutableList<Film>) {
+    val ctx = LocalContext.current
+    var visibleAlertDialog by remember {
+        mutableStateOf(false)
+    }
+    var filmSelected by remember {
+        mutableStateOf(0)
+    }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxHeight(0.85f)
+            .padding(10.dp)
+    ) {
 
-    LazyColumn(modifier = Modifier
-        .fillMaxHeight(0.85f)
-        .padding(10.dp)) {
+        items(films, key = { film -> film.id }) { film ->
+            Column(
+                modifier = Modifier
+                    .clickable {
 
-        items(films) {
-            val film=it
-            Column(modifier = Modifier
-                .clickable {
-                    val route = NavDestination.DetailsFilm.createRoute(it.id)
-                    navController.navigate(route = route)
-                }
-                .fillMaxWidth()
-                .height(80.dp)
+                    }
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .pointerInput(Unit) {
+                        detectTapGestures(
+                            onTap = {
+                                val route = NavDestination.DetailsFilm.createRoute(film.id)
+                                navController.navigate(route = route)
+                            },
+                            onLongPress = {
+                                visibleAlertDialog = true
+                                filmSelected = films.getFilmById(film.id)
+                            }
+                        )
+                    }
+
             ) {
+
                 Row(
                     horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier
@@ -82,9 +105,12 @@ fun FilmList(navController: NavController, films: List<Film>) {
                         .padding(1.dp)
                 ) {
 
-                    Text(text = "${film.nazwa}", modifier = Modifier.align(Alignment.CenterVertically))
+                    Text(
+                        text = "${film.nazwa}",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
 
-                    Row(modifier = Modifier.fillMaxHeight()){
+                    Row(modifier = Modifier.fillMaxHeight()) {
                         Text(text = "${film.rating}", modifier = Modifier.align(Alignment.Bottom))
                         Icon(
                             Icons.Default.Star,
@@ -103,4 +129,19 @@ fun FilmList(navController: NavController, films: List<Film>) {
 
 
     }
+    if (visibleAlertDialog)
+        AlertDialog(
+            onDismissRequest = { visibleAlertDialog = false },
+            title = { Text(text = ctx.getString(R.string.delete_confirmation)) },
+            confirmButton = {
+                Button(onClick = {
+
+                    val index = filmSelected
+                    films.removeAt(index)
+                    visibleAlertDialog = false
+                }) {
+                    Text(text = ctx.getString(R.string.button_delete))
+                }
+            }
+        )
 }
