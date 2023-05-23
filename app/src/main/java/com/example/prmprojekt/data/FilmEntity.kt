3fa9e4b6cb40c477e99f884aa88191e954d1adcd
CodeServant <49171565+CodeServant@users.noreply.github.com>
2023-05-23 16:47:06 +0200

    create FilmEntity

---
+++ app/src/main/java/com/example/prmprojekt/data/FilmEntity.kt
@@ -0,0 +1,16 @@
+package com.example.prmprojekt.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.math.BigDecimal
+
+@Entity
+class FilmEntity(
+    var nazwa: String = "",
+    var rating: BigDecimal? = null,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var url: String = ""
+) {
+}
