2eea1edf1a1531d04ffb0b1afc4b3e24dbdecacc
CodeServant <49171565+CodeServant@users.noreply.github.com>
2023-05-26 12:14:05 +0200

    apply BigDecimalTypeConverter in FilmDatabase

--- app/src/main/java/com/example/prmprojekt/data/FilmDatabase.kt
+++ app/src/main/java/com/example/prmprojekt/data/FilmDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [FilmEntity::class], version = 1)
+@TypeConverters(BigDecimalTypeConverter::class)
 abstract class FilmDatabase : RoomDatabase() {
     abstract val filmDAO: FilmDAO
 
