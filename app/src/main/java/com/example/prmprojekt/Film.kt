a2ead9ed5ae94875fe55d258584da82bb53cf3ac
CodeServant <49171565+CodeServant@users.noreply.github.com>
2023-06-06 16:26:46 +0200

    validate BigDecimal if it meets the buisiness requirements

--- app/src/main/java/com/example/prmprojekt/Film.kt
+++ app/src/main/java/com/example/prmprojekt/Film.kt
@@ -2,4 +2,11 @@ package com.example.prmprojekt
 
 import java.math.BigDecimal
 
-data class Film (var nazwa: String="", var rating: BigDecimal?=null, var id: Int, var url: String = "")
+data class Film (var nazwa: String="", var rating: BigDecimal?=null, var id: Int, var url: String = ""){
+
+}
+fun isRating(rating : BigDecimal?):Boolean{
+    if(rating == null) return false
+    var valid = rating >= BigDecimal(1) && rating<=BigDecimal(10)
+    return valid
+}
